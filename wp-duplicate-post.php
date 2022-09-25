1efeea034626dbfc00f1772110ce9a7a886e713d
Vasyl Sokolyk <vsokolyk@gmail.com>
2022-09-25 20:57:06 +0300

    copy files from wp-performance

---
+++ wp-duplicate-post.php
@@ -0,0 +1,21 @@
+<?php
+
+use JazzMan\WpDuplicatePost\Duplicate;
+
+/**
+ * Plugin Name:         wp-duplicate-post
+ * Plugin URI:          https://github.com/Jazz-Man/wp-duplicate-post
+ * Description:         Duplicate Posts, Pages and Custom Posts easily using single click. You can duplicate your pages, posts and custom post by just one click and it will save as your selected options (draft, private, public, pending).
+ * Author:              Vasyl Sokolyk
+ * Author URI:          https://www.linkedin.com/in/sokolyk-vasyl
+ * Requires at least:   5.2
+ * Requires PHP:        7.4
+ * License:             MIT
+ * Update URI:          https://github.com/Jazz-Man/wp-duplicate-post.
+ */
+
+if (function_exists('app_autoload_classes') && class_exists(Duplicate::class)) {
+    app_autoload_classes([
+        Duplicate::class,
+    ]);
+}
