2454efdcbee9c19463acc51b82eb3af4d8a43cf5
Vasyl Sokolyk <6892898+Jazz-Man@users.noreply.github.com>
2022-12-04 23:13:06 +0200

    Update wp-duplicate-post.php

--- wp-duplicate-post.php
+++ wp-duplicate-post.php
@@ -14,7 +14,6 @@ use JazzMan\WpDuplicatePost\Duplicate;
  * Update URI:          https://github.com/Jazz-Man/wp-duplicate-post.
  */
 
-
 if (function_exists('app_autoload_classes') && class_exists(Duplicate::class)) {
     app_autoload_classes([
         Duplicate::class,
